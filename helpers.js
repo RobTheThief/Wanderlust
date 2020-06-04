22fa0f960c392271606e96b9753f5e17d731b461
Robert Gannon <cyberpeaches@gmail.com>
2020-06-04 12:41:05 +0100

    Added best photo of venues

--- helpers.js
+++ helpers.js
@@ -4,20 +4,17 @@ const createVenueHTML = (name, location, iconSource) => {
   <h3>Address:</h3>
   <p>${location.address}</p>
   <p>${location.city}</p>
-  <p>${location.country}</p>
-  <p>Latitude: ${roundNum(location.lat)}</p>`;
-
+  <p>${location.country}</p>`;
 }
 
 const createWeatherHTML = (currentDay) => {
   console.log(currentDay)
   return `<h2>${weekDays[(new Date()).getDay()]}</h2>
-		<h2>Temperature: ${kelvinToCelsius(currentDay.main.temp)}&deg;C</h2>
+		<h2>Temperature: ${roundNum(currentDay.main.temp)}&deg;C</h2>
 		<h2>Condition: ${currentDay.weather[0].description}</h2>
     <h2>Wind Speed: ${msToKph(currentDay.wind.speed)}Kph</h2>
   	<img src="https://openweathermap.org/img/wn/${currentDay.weather[0].icon}@2x.png">`;
 }
 
-const kelvinToCelsius = k => (k.toFixed(0));
-const msToKph = m => (m * 3.6);
+const msToKph = m => ((m * 3.6).toFixed(0));
 const roundNum = n => (n.toFixed(0));
--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const clientId = '34UUSFD0MZLRSLRFSSHCF4D0K5QYIGYYQRN1Y0AMXPDOPBLJ';
 const clientSecret = 'XI1LHWZZMEOVCCJVYRQPELVHA3MTTWXZOAWGPDQEXXICTYAG';
 const url = 'https://api.foursquare.com/v2/venues/explore?near=';
+const url2 = 'https://api.foursquare.com/v2/venues/';
 
 // OpenWeather Info
 const openWeatherKey = '3f7ff6c50748a60b4491e44faaa808ca';
@@ -16,7 +17,7 @@ const $venueDivs = [$("#venue1"), $("#venue2"), $("#venue3"), $("#venue4")];
 const $weatherDiv = $("#weather1");
 const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-// Add AJAX functions here:
+//AJAX functions:
 const getVenues = async () => {
   const city = $input.val();
   const urlToFetch = `${url}${city}&limit=10&client_id=${clientId}&client_secret=${clientSecret}&v=20200512`;
@@ -49,7 +50,6 @@ function shuffle(array) {
     array[currentIndex] = array[randomIndex];
     array[randomIndex] = temporaryValue;
   }
-
   return array;
 }
 
@@ -68,37 +68,81 @@ const getForecast = async () => {
     }
 }
 
-
 // Render functions
-const renderVenues = (venues) => {
-  // Randomizes venues
-  shuffle(venues);
-
-  $venueDivs.forEach(($venue, index) => {
-    // Add your code here:
-    const venue = venues[index];
-      const venueIcon = venue.categories[0].icon;
-      console.log(venueIcon);
-      const venueImgSrc = `${venueIcon.prefix}bg_64${venueIcon.suffix}`;
-      let venueContent = createVenueHTML(venue.name, venue.location, venueImgSrc);
-    $venue.append(venueContent);
-  });
-  $destination.append(`<h2>${venues[0].location.city}</h2>`);
-}
-
 const renderForecast = (day) => {
-  // Add your code here:
-
 	let weatherContent = createWeatherHTML(day);
   $weatherDiv.append(weatherContent);
 }
+
+const renderVenues = (venues) => {
+
+  $venueDivs.forEach(($venue, index) => {
+    const venue = venues[index];
+      //const venueIcon = venue.categories[0].icon;
+
+      const venueImgSrc = `${venue.bestPhoto.prefix}128x128${venue.bestPhoto.suffix}`;
+      let venueContent = createVenueHTML(venue.name, venue.location, venueImgSrc);
+    $venue.append(venueContent);
+  });
+  $destination.append(`<h2>${venues[0].location.city}<br><span style = "font-size: 14px">Latitude: ${roundNum(venues[0].location.lat)}&deg;</span></h2>`);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+const getVenueDetails = async id => {
+    const urlToFetch = `${url2}${id}?client_id=${clientId}&client_secret=${clientSecret}&v=20200512`;
+    try{
+      const response = await fetch(urlToFetch);
+      if(response.ok){
+        const jsonResponse = await response.json();
+        details = jsonResponse.response.venue;
+        return details;
+      }
+    } catch(error){console.log(error);}
+};
+
+
+/*
+  const renderVenues = (array) => {
+    //console.log('Render');
+    //console.log(details);
+    array.forEach(item => console.log(`${item.bestPhoto.prefix}128x256${item.bestPhoto.suffix}`));
+    debugger;
+
+    $venueDivs.forEach(($venue, index) => {
+      const venueIcon = array[index].categories[0].icon;
+      const venueImgSrc = `${venueIcon.prefix}bg_64${venueIcon.suffix}`;//the photo i wanna use
+
+
+      let venueContent = createVenueHTML(venue.name, venue.location, venueImgSrc);
+      $venue.append(venueContent);
+    });
+    $destination.append(`<h2>${venues[0].location.city}<br><span style = "font-size: 14px">Latitude: ${roundNum(venues[0].location.lat)}&deg;</span></h2>`);
+  };
+
+*/
+////////////////////////////////////////////////////////////////////////////////////////
+
 
 const executeSearch = () => {
   $venueDivs.forEach(venue => venue.empty());
   $weatherDiv.empty();
   $destination.empty();
   $container.css("visibility", "visible");
-  getVenues().then(venues => renderVenues(venues));
+  getVenues()
+    .then(async venues => {
+      let detailsArray = [];
+      shuffle(venues);
+      for(let i = 0; i < 3; i++){ //Can't use forEach
+        detailsArray.push(await getVenueDetails(venues[i].id));
+      }
+      //console.log(detailsArray);
+
+      return renderVenues(detailsArray);
+    });
+  //getVenues().then(venues => renderVenues(venues));
   getForecast().then(forecast => renderForecast(forecast));
   return false;
 }
